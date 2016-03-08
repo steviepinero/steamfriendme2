66be564fa79949b1f3fb15cf15fbfc987f896c97
SteveC <stevechenweb@gmail.com>
2016-03-08 09:44:24 -0500

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308020506) do
+ActiveRecord::Schema.define(version: 20160308021627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.string  "game_name"
-    t.string  "appid"
-    t.integer "playtime"
-    t.string  "img_icon_url"
-    t.string  "user_id"
+    t.string   "game_name"
+    t.string   "appid"
+    t.string   "playtime"
+    t.string   "img_icon_url"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
+
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
@@ -35,4 +39,5 @@ ActiveRecord::Schema.define(version: 20160308020506) do
     t.string "provider"
   end
 
+  add_foreign_key "games", "users"
 end
